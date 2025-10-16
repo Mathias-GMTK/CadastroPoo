7cfa0b2d8b3711efab267f33a029302bb924d2b3
Caio <caiodesouza.cds@gmail.com>
2025-10-16 20:44:00 -0300

    Getters Setter Filme

--- src/main/java/cadastro/com/poo/entity/Filme.java
+++ src/main/java/cadastro/com/poo/entity/Filme.java
@@ -2,12 +2,70 @@ package cadastro.com.poo.entity;
 
 import jakarta.persistence.Entity;
 
+import java.time.LocalDate;
+
 @Entity
 public class Filme {
 
     private Long id;
     private String nome;
-    private String categoria;
+    private String genero;
+    private Long duracao;
+    private String sinopse;
+    private LocalDate dataLancamento;
 
-public Long
+    public Filme() {
+    }
+
+    public Filme(String nome, String genero, Long duracao, String sinopse, LocalDate dataLancamento) {
+        this.nome = nome;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Long getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(Long duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
 }
