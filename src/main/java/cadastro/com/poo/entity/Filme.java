a7fea97122da71ca29997addced17aaefe7de129
1175015765 <mathigmtk@gmail.com>
2025-10-16 20:19:16 -0300

    first

--- src/main/java/cadastro/com/poo/entity/Filme.java
+++ src/main/java/cadastro/com/poo/entity/Filme.java
@@ -9,5 +9,27 @@ public class Filme {
     private String nome;
     private String categoria;
 
-public Long
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
