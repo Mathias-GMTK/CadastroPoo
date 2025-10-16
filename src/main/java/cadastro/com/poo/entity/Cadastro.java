3c90c23fd0b4365f295bf258bf6a627e11b1bb4f
MATHIAS GRUBER MARTINHUNK <mathias.martinhunk@univille.br>
2025-10-16 20:15:16 -0300

    Initial commit

---
+++ src/main/java/cadastro/com/poo/entity/Cadastro.java
@@ -0,0 +1,4 @@
+package cadastro.com.poo.entity;
+
+public class Cadastro {
+}
---
+++ src/main/java/cadastro/com/poo/entity/Filme.java
@@ -0,0 +1,13 @@
+package cadastro.com.poo.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Filme {
+
+    private Long id;
+    private String nome;
+    private String categoria;
+
+public Long
+}
