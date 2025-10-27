d8afdba5c5e13747a56a8c344087497e81fdc0df
P1R1L4NP0 <carloslanser1@gmail.com>
2025-10-26 22:57:24 -0300

    add

--- src/main/java/cadastro/com/poo/controller/FilmeController.java
+++ src/main/java/cadastro/com/poo/controller/FilmeController.java
@@ -1,12 +1,10 @@
 package cadastro.com.poo.controller;
 
-
+import cadastro.com.poo.dto.CriarFilmeRequest;
+import cadastro.com.poo.dto.EditarFilmeRequest;
 import cadastro.com.poo.service.FilmeService;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,15 +13,48 @@ public class FilmeController {
 
     private final FilmeService filmeService;
 
-    public FilmeController(FilmeService filmeService){
+    public FilmeController(FilmeService filmeService) {
         this.filmeService = filmeService;
     }
 
     @GetMapping
-    public ModelAndView index(){
-        var mv = new ModelAndView("filme/index");
+    public ModelAndView index() {
+        var mv = new ModelAndView("filmes/index");
         var listaDeFilmes = filmeService.findAll();
-        mv.addObject("Filmes", listaDeFilmes);
+        mv.addObject("filmes", listaDeFilmes);
         return mv;
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView newFilmeForm() {
+        var mv = new ModelAndView("filmes/novo");
+        mv.addObject("filme", new CriarFilmeRequest(null, null, null, null, null));
+        return mv;
+    }
+
+    @PostMapping
+    public String createFilme(CriarFilmeRequest request) {
+        filmeService.criar(request);
+        return "redirect:/filmes";
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editFilmeForm(@PathVariable Long id) {
+        var mv = new ModelAndView("filmes/editar");
+        var filme = filmeService.obterPeloId(id).orElseThrow(() -> new RuntimeException("Filme não encontrado"));
+        mv.addObject("filme", new EditarFilmeRequest(filme.getId(), filme.getNome(), filme.getGenero(), filme.getDuracao(), filme.getSinopse(), filme.getDataLancamento()));
+        return mv;
+    }
+
+    @PutMapping("/{id}")
+    public String updateFilme(@PathVariable Long id, EditarFilmeRequest request) {
+        filmeService.editar(request);
+        return "redirect:/filmes";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteFilme(@PathVariable Long id) {
+        filmeService.deletarPeloId(id);
+        return "redirect:/filmes";
     }
 }
--- src/main/java/cadastro/com/poo/util/AppConfiguration.java
+++ src/main/java/cadastro/com/poo/util/AppConfiguration.java
@@ -1,6 +1,5 @@
 package cadastro.com.poo.util;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
